74d9e720b1a50b84b5ac0da1f16af003916d8506
jevons0411 <jevons0411@163.com>
2022-09-23 11:11:02 +0800

    Update SrcAuto.js

--- SrcAuto.js
+++ SrcAuto.js
@@ -855,9 +855,12 @@ var SrcParseS = {
                         var tstime = urlcode.body.match(/#EXT-X-TARGETDURATION:(.*?)\n/)[1];
                         var urltss = urlcode.body.replace(/#.*?\n/g,'').replace('#EXT-X-ENDLIST','').split('\n');
                     }catch(e){
+                        log(e.message);
                         var tstime = 0;
                         var urltss = [];
                     }
+                    log(tstime);
+                    log(urltss);
                     if(parseInt(tstime)*parseInt(urltss.length) < times){
                         log(name+'>√m3u8视频长度小于'+times+'s，疑似跳舞小姐姐或防盗小视频，不信去验证一下>'+url);
                         return 0;
