034ecb8702cdb6c41c2e47db857431a28d250fab
src48597962 <48597962@qq.com>
2022-10-21 12:25:09 +0800

    Update require.js

--- require.js
+++ require.js
@@ -1,10 +1,17 @@
 let file1 = "https://gitcode.net/src48597962/hk/-/raw/master/SrcJuying.js";
 let file2 = "https://agit.ai/src48597962/Src/raw/branch/master/SrcJuying.js";
-if(!fileExist('hiker://files/libs/' + md5(file1) + '.js' && !fileExist('hiker://files/libs/' + md5(file2) + '.js')){
-  let cjFile = request(file2,{timeout:2000});
+let relyfile = "";
+if(fileExist('hiker://files/libs/' + md5(file1) + '.js'){
+  relyfile = file1;
+}else if(fileExist('hiker://files/libs/' + md5(file2) + '.js'){
+  relyfile = file2;
+}
+if(relyfile==""){
+  let cjFile = request(file1,{timeout:2000});
   if(cjFile.indexOf('nowVersion') > -1){
-    var relyfile = file2;
+    relyfile = file1;
   }else{
-    var relyfile = file1;
+    relyfile = file2;
   }
 }
+  
