12002fdccb13806eec22d1732d771bc6f39c96a7
src48597962 <48597962@qq.com>
2022-10-21 12:48:48 +0800

    Update require.js

--- require.js
+++ require.js
@@ -1,9 +1,9 @@
 let file1 = "https://gitcode.net/src48597962/hk/-/raw/master/SrcJuying.js";
 let file2 = "https://agit.ai/src48597962/Src/raw/branch/master/SrcJuying.js";
 var relyfile = "";
-if(fileExist('hiker://files/libs/' + md5(file1) + '.js'){
+if(fileExist('hiker://files/libs/' + md5(file1) + '.js')){
   relyfile = file1;
-}else if(fileExist('hiker://files/libs/' + md5(file2) + '.js'){
+}else if(fileExist('hiker://files/libs/' + md5(file2) + '.js')){
   relyfile = file2;
 }
 if(relyfile==""){
