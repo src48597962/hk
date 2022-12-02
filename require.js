5af938f7211e5eab9db85e0a5ac8260907743e65
src48597962 <48597962@qq.com>
2022-12-02 19:57:16 +0800

    Update require.js

--- require.js
+++ require.js
@@ -1,9 +1,12 @@
 let file1 = "https://gitcode.net/src48597962/hk/-/raw/master/SrcJuying.js";
-let file2 = "https://jihulab.com/src485979621/hk/-/raw/master/SrcJuying.js";
+let file2 = "https://agit.ai/src48597962/Src/raw/branch/master/SrcJuying.js";
+let file3 = "https://jihulab.com/src485979621/hk/-/raw/master/SrcJuying.js";
 var relyfile = "";
 if(fileExist('hiker://files/libs/' + md5(file1) + '.js')){
   relyfile = file1;
 }else if(fileExist('hiker://files/libs/' + md5(file2) + '.js')){
+  relyfile = file2;
+}else if(fileExist('hiker://files/libs/' + md5(file3) + '.js')){
   relyfile = file2;
 }
 if(relyfile==""){
@@ -11,7 +14,12 @@ if(relyfile==""){
   if(cjFile.indexOf('nowVersion') > -1){
     relyfile = file1;
   }else{
-    relyfile = file2;
+    let cjFile = request(file2,{timeout:2000});
+    if(cjFile.indexOf('nowVersion') > -1){
+      relyfile = file2;
+    }else{
+      relyfile = file3;
+    }
   }
 }
   
