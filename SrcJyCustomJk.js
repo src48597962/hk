0d8c267ae6794f6b11e74c0c2448b3ed5c158904
src48597962 <48597962@qq.com>
2022-12-04 19:08:16 +0800

    Update SrcJyCustomJk.js

--- SrcJyCustomJk.js
+++ SrcJyCustomJk.js
@@ -90,5 +90,47 @@ let customparse = {
             });
         }
         return list;
+    },
+    csp_custom_aiwandou: function (name) {
+        try {
+            var lists = [];
+            let html = request("https://wuli.api.bailian168.cc/movie/getsearchlist/keywords/"+name+"/page/1/rows/20.json");
+            let data = JSON.parse(html).data;
+            data.forEach(item=>{
+                let ainame = item.movie_name;
+                if(ainame == name){
+                    let aiurl = "https://www.wandou.pro/_next/data/7cd38774e4afd0127c31c9ea7e3835dd76514e15/detail/"+item.movie_id+".json?id="+item.movie_id;
+                    let aipic = item.movie_img_url;
+                    let aihtml = request(aiurl);
+                    let htmls = JSON.parse(aihtml).pageProps.data.playData;
+                    let vodname = JSON.parse(aihtml).pageProps.data.movie.movie_name;
+                    htmls.forEach(it=>{
+                        try{
+                            let sitename = it.site_name;
+                            let vodurl = it.data_url;
+                            if(!lists.some(ii => ii.url==vodurl)){
+                                lists.push({name:vodname,pic:aipic,url:vodurl,site:sitename});
+                            }
+                        }catch(e){}
+                    })
+                }
+            })
+        } catch (e) {
+            log(e.message);
+            var lists = [];
+        }
+
+        let list = [];
+        lists.forEach(item=>{
+            if(!/qq|mgtv|iptv|iqiyi|youku|bilibili|souhu|cctv|icaqd|cokemv|mhyyy|fun4k|jpys\.me|31kan|37dyw|kpkuang/.test(item.url)&&!list.some(ii => ii.vodurl==item.url)){
+                list.push({
+                    vodname: item.name,
+                    vodpic: item.pic.replace(/http.*?\?url=/,''),
+                    voddesc: item.site,
+                    vodurl: item.url
+                })
+            }
+        })
+        return list;
     }
 }
