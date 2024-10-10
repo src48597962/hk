e512208cbc96ee897c8d73f1cbf81d00597b1575
src48597962 <48597962@qq.com>
2024-10-10 14:29:32 +0800

    Update require.js

--- require.js
+++ require.js
@@ -2,25 +2,15 @@ let file1 = "https://codeberg.org/src48597962/hk/raw/branch/Ju/SrcJu.js";
 let file2 = "https://gitcode.net/src48597962/hk/-/raw/Ju/SrcJu.js";
 let file3 = "https://agit.ai/src48597962/hk/raw/branch/Ju/SrcJu.js";
 
-let relyfile = "";
-if(fileExist('hiker://files/libs/' + md5(file1) + '.js')){
+let cjFile = request(file1,{timeout:3000});
+if(cjFile.indexOf('nowVersion') > -1){
   relyfile = file1;
-}else if(fileExist('hiker://files/libs/' + md5(file2) + '.js')){
-  relyfile = file2;
-}else if(fileExist('hiker://files/libs/' + md5(file3) + '.js')){
-  relyfile = file2;
-}
-if(relyfile==""){
-  let cjFile = request(file1,{timeout:3000});
+}else{
+  let cjFile = request(file2,{timeout:3000});
   if(cjFile.indexOf('nowVersion') > -1){
-    relyfile = file1;
+    relyfile = file2;
   }else{
-    let cjFile = request(file2,{timeout:3000});
-    if(cjFile.indexOf('nowVersion') > -1){
-      relyfile = file2;
-    }else{
-      relyfile = file3;
-    }
+    relyfile = file3;
   }
 }
   
