7f0a765e28517e75d20c7215f893a042ce3e19fd
src48597962 <48597962@qq.com>
2025-05-30 10:23:44 +0800

    Update JuRequire.js

--- JuRequire.js
+++ JuRequire.js
@@ -1,6 +1,6 @@
 let file1 = "https://raw.gitcode.com/src48597962/juyue/raw/master/SrcJu.js";
 let file2 = "https://codeberg.org/src48597962/Juyue/raw/branch/master/SrcJu.js";
-let file3 = "https://raw.githubusercontent.com/src48597962/Juyue/master/SrcJuying.js";
+let file3 = "https://raw.githubusercontent.com/src48597962/Juyue/master/SrcJu.js";
 
 let cjFile = request(file1,{timeout:3000});
 if(cjFile.indexOf('nowVersion') > -1){
